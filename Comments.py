17661ac20e65371a5da6a05824fa49c09e693a5a
Andrew Streaman <andrewstreaman@gmail.com>
2019-03-16 14:24:40 -0400

    Made some changes to the read.md

---
+++ Comments.py
@@ -0,0 +1,12 @@
+print('Exit Classroom')
+print('Enter stairwell')
+print('Walk downstairs')
+print('Press elevator button to go down floors')
+print('Enter elevator')
+print('Press lobby button')
+# Lobby button could also be the first floor
+print('If door opens prior to reaching first floor, THEN remain in elevator')
+print('When elevator door opens to the first floor, exit')
+print('Walk towards door leading to the sidewalk')
+print('Open door')
+print('Step outside and turn to the right')

bc045ed6f28a44b4181b6d4b9e4f8dfc809e2169
DomesticWorkAssault <andrewstreaman@gmail.com>
2019-03-30 16:08:35 -0400

    Initial commit

---
+++ Unit1/area_of_rectangle.py
@@ -0,0 +1,14 @@
+"""
+The purpose of this program is to calculate the area of a rectangle.
+The formula to calculate the area of a rectangle is length multiplied by width
+a = lw
+"""
+length = float(input("Please input the length in inches"))
+width = float(input("Please input the width in inches"))
+
+# length = 10.0
+# width = 10.0
+
+area = length*width
+
+print("The area of the rectangle is calculated as", area, "inches")
